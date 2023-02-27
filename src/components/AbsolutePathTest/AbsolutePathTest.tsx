57de1d9ece6bc3a6983a60a47fa102d78fab1052
hiepdeptrai0908 <hiepdeptrai0908@gmail.com>
2023-02-28 04:44:26 +0900

    fist setting path

---
+++ src/components/AbsolutePathTest/AbsolutePathTest.tsx
@@ -0,0 +1,5 @@
+function AbsolutePathTest() {
+    return <h1>Test đường dẫn tuyệt đối</h1>
+}
+
+export default AbsolutePathTest
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import path from 'path'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,8 +13,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '~': '/src'
-            // src: path.resolve(__dirname, './src')
+            '~': path.resolve(__dirname, '/src')
         }
     }
 })
